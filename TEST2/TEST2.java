50eea21ea6696b2cf2e084b1d182c6b891117dcc
Jaehyeong Ha <37164351+HaJaehyeong@users.noreply.github.com>
2018-09-12 12:16:50 +0900

    20180912

---
+++ TEST2/TEST2.java
@@ -0,0 +1,39 @@
+public class TEST2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int solution(int N) {
+		
+		// String To Binary Num
+		String bin = Integer.toBinaryString(N); 
+		
+		int count_one = 0;
+		int count_zero = 0;
+		int saveCount_zero = 0;
+		
+		// Check
+		for (int i = 0 ; i < bin.length() ; i++) {
+			if (bin.charAt(i) == '1'){
+				count_one++;
+				if (saveCount_zero < count_zero) {
+					saveCount_zero = count_zero;
+				}
+				count_zero = 0;
+			} else if (bin.charAt(i) == '0') {
+				count_zero++;
+			} 
+		}
+		
+		// return
+		if (count_one == 1) {
+			return 0;
+		} else {
+			return saveCount_zero;
+		}
+		
+	}
+}
+
